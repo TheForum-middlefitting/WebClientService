63ef40c5f697c004e51d3001957402f14293034e
middlefitting <wjdtmdcjf2@gmail.com>
2023-02-17 02:13:50 +0900

    FEAT: 대표사진 및 favicon 설정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,45 +1,9 @@
-import { TagCloud } from 'react-tagcloud'
-import classes from "./Home.module.css"
+import MainImage from "../img/MainImage.png"
 
-const data = [
-    { value: 'Java 11', count: 38 },
-    { value: 'Spring Boot', count: 38 },
-    { value: 'JUnit 5', count: 30 },
-    { value: 'JPA', count: 30 },
-    { value: 'React.js', count: 30 },
-    { value: 'Javascript', count: 30 },
-    { value: 'Nodejs', count: 28 },
-    { value: 'QueryDsl', count: 27 },
-    { value: 'REST API', count: 25 },
-    { value: 'MicroService', count: 25 },
-    { value: 'Docker', count: 25 },
-    { value: 'Jenkins', count: 22 },
-    { value: 'MySQL', count: 18 },
-    { value: 'TypeScript', count: 17 },
-    { value: 'JWT', count: 15 },
-    { value: 'GitHub', count: 15 },
-    { value: 'AWS', count: 15 },
-    { value: 'REST Docs', count: 11 },
-    { value: 'JetBrains', count: 7 },
-]
-
-const customRenderer = (tag, size, color) => (
-    <span
-        key={tag.value}
-        className={classes.wordCloud}
-        style={{
-            animationDelay: `${Math.random() * 2}s`,
-            fontSize: `${size * 1.5}em`,
-            border: `2px solid ${color}`,
-        }}
-    >
-    {tag.value}
-  </span>
-)
 export default function Home() {
     return (
-        <div className={classes.auth}>
-            <TagCloud tags={data} minSize={1} maxSize={5} renderer={customRenderer} onClick={tag => alert(`'${tag.value}' was selected!`)}/>
+        <div align={"center"}>
+            <img className="phoneImage" alt="iPhone_01" src={MainImage} height={800}/>
         </div>
     )
 }
